6455442b52b2f22a584104df03f1d15b0e2da41b
cesarisern <cesarisern@gmail.com>
2018-07-11 00:11:28 +0100

    custom-Form file missing

---
+++ js/custom-Form.js
@@ -0,0 +1,30 @@
+function validateCustomForm(email) {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) { //Add better email verification ;-)
+    console.log("Would pass");
+
+    mixpanel.identify();
+    mixpanel.people.set({ "$email": email });
+
+    popupForm = window.typeformEmbed.makePopup("https://cesar231.typeform.com/to/H5joQp?email=" + email, {
+      mode: 'drawer_left',
+      autoClose: 1,
+      hideHeaders: true,
+      hideFooter: true
+    });
+
+    popupForm.open();
+
+  } else {
+
+    // I'm using CSS animations because JQuery needs a plugin to animate color, yet I use JQuery to detect the end of the animation and then remove the animation class
+    document.getElementById('form-underline').classList.add("error-animation-solid");
+    document.getElementById('formValueId1').classList.add("error-animation-text");
+    document.getElementById('formValueId2').classList.add("error-animation-text");
+    $("#formValueId1").bind("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd",
+      function() {
+        document.getElementById('form-underline').classList.remove("error-animation-solid");
+        document.getElementById('formValueId1').classList.remove("error-animation-text");
+        document.getElementById('formValueId2').classList.remove("error-animation-text");
+      });
+  }
+}
